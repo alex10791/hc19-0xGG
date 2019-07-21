510f7b7ad64cfbec367ad883d766df60bafb0c9a
Nicolas Tsagarides <nicexe@gmail.com>
2019-07-21 16:39:55 +0300

    add button control code

---
+++ hardware/button_control/button_control.ino
@@ -0,0 +1,43 @@
+
+#include "Keyboard.h"
+
+
+const int buttonPin = 2;    // the number of the pushbutton pin
+
+// Variables will change:
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      if (buttonState == LOW) {
+        Keyboard.write('u');
+      }
+    }
+  }
+
+  lastButtonState = reading;
+}
