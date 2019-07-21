c37a8cfca1cd61cd1da787ac4cb1e739d78ab750
Nicolas Tsagarides <nicexe@gmail.com>
2019-07-21 11:49:40 +0300

    fix whitespace

--- backend/hardware/relay_controlls.py
+++ backend/hardware/relay_controlls.py
@@ -1,14 +1,14 @@
 from gpiozero import LED
-    
+
 power_enabled_pin = LED(23)
 
 def enable_power():
     power_enabled_pin.on()
-    
-    
+
+
 def disable_power():
     power_enabled_pin.off()
-    
+
 
 if __name__ == '__main__':
     import time
